0060e15b9d9a7bcd3b8106d44c21afaf6b043ca5
yesdani <yesdaniece@gmail.com>
2023-09-10 09:49:33 +0100

    redis service

--- redis.sh
+++ redis.sh
@@ -23,26 +23,25 @@ VALIDATE(){
    if [ "$1" -ne 0 ];
       then
         echo -e " $2....$R Failure $N"
-        exit 1
       else 
       echo -e "   $2 .... $G Success $N"
    fi
 }
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOGFILE
+  
 VALIDATE $? 'Installing redis repo'
 
-yum module enable redis:remi-6.2 -y
+yum module enable redis:remi-6.2 -y &>>$LOGFILE
 VALIDATE $? 'Enabling redis 6.2'
 
-yum install redis -y 
+yum install redis -y &>>$LOGFILE
 VALIDATE $? 'Installing redis '
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf
-VALIDATE $? 'Changing  in redis.conf'
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf &>>$LOGFILE
+VALIDATE $? 'Allowing remote connections t0 redis'
 
-systemctl enable redis
+systemctl enable redis &>>$LOGFILE
 VALIDATE $? 'enabling  redis '
 
-systemctl start redis
+systemctl start redis &>>$LOGFILE
 VALIDATE $? 'starting  redis '
