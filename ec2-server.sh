3bd5aa54a259439036e3cc22fb95602674c8a7ee
yesdani <yesdaniece@gmail.com>
2023-10-23 09:25:46 +0100

    inventory playbook

--- ec2-server.sh
+++ ec2-server.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-
-Name=$@
+Names=$@
 IMAGEID=ami-03265a0778a880afb 
 SECURITYGROUGID=sg-08105091605f3144d 
 INSTACETYPE=""
 HOSTEDZONE=Z05740803UNSE6BOTDZEC
 DOMAIN_NAME=joindevops.top
-for i in $@
- do
+for i in '$@'
+do
   if [[ $i == "mongo" || $i == "mysql" ]]
-   then 
+  then 
        INSTACETYPE="t2.small"
     else
        INSTACETYPE="t2.micro"
  fi
 
  IpAddress=$(aws ec2 run-instances --image-id $IMAGEID --count 1 --instance-type $INSTACETYPE --security-group-ids $SECURITYGROUGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
- echo "creating instance $i $IpAddress"
+ echo "creating instance $i $IpAddress $Names"
 
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE --change-batch file "
+#aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE --change-batch file "
 # {
 #                 " Comment ": " CREATE ",
 #                  " Changes ": [ {
 #                              " Action ": " CREATE ",
-#                             " ResourceRecordSet ": {
+#                             " ResourceRecordSet ": { 
 #                                 " Name ": " $i.$DOMAIN_NAME ",
 #                                     " Type ": " A ",
 #                                      " TTL ": 300,
